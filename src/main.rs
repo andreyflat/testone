b20243b9193934174285800aea516ba06463b626
andreyflat <andrey.flat.post@gmail.com>
2025-03-17 21:55:11 +0800

    start testone

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(spawn_camera)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera3dBundle::default());
+}
+fn spawn_scene(mut commands: Commands) {
+    // Куб
+    commands.spawn(PbrBundle {
+        mesh: Mesh::from(shape::Cube { size: 1.0 }),
+        material: StandardMaterial {
+            base_color: Color::rgb(0.8, 0.2, 0.2),
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+
+    // Свет
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+}
